3361338080f8c259326a1c31e5dd71476eb90839
sahil shetker <68986333+Shad0w-0@users.noreply.github.com>
2022-10-25 10:37:16 +0530

    Add files via upload

---
+++ selection.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter number of elements to be entered"<<endl;
+	cin>>n;
+	
+	int arr[n];
+	cout<<"Enter elements"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			if(arr[j]<arr[i])
+			{
+				int temp=arr[j];
+				arr[j]=arr[i];
+				arr[i]=temp;
+				
+			}
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<"";
+	}
+	cout<<endl;
+	
+}
