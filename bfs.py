22d1a4c04b98b28234768db53a491fdf6da32ba2
sahil shetker <68986333+sahilshetker@users.noreply.github.com>
2024-06-21 14:40:15 +0530

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -1,1 +1,34 @@
-bfs
+visited = []
+queue = []
+
+def bfs(visited,graph,node):
+	visited.append(node)
+	queue.append(node)
+	
+	while queue:
+		m=queue.pop(0)
+		print(m,end="")
+		
+		for side in graph[m]:
+			if side not in visited:
+				visited.append(side)
+				queue.append(side)
+
+def input_graph():
+    graph = {}
+    n = int(input("Enter the number of nodes in the graph: "))
+    
+    for _ in range(n):
+        node = input("Enter the node: ")
+        edges = input(f"Enter the edges for {node} separated by space: ").split()
+        graph[node] = edges
+    
+    return graph
+
+
+# Take the graph input from the user
+graph = input_graph()
+
+# Start BFS traversal from the specified start node
+start_node = input("Enter the start node for BFS traversal: ")
+bfs(visited, graph, start_node)
